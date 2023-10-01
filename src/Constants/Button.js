3669ac10e820c7a34b757e07b7134525fd0390d0
Kaviruban Mohanasundaram <ruban7154@gmail.com>
2023-10-01 17:52:30 +0530

    initial-updates

---
+++ src/Constants/Button.js
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+export const AddToCartBtn = styled.button`
+  width: 200px;
+  /* margin-left: 20px; */
+  height: 35px;
+  /* width: 150px; */
+  background-color: black;
+  border: none;
+  border-radius: 2px;
+  color: white;
+  padding: 5px;
+  cursor: pointer;
+`;
---
+++ src/Constants/Images.js
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+export const StyleImage = styled.img`
+  height: 250px;
+  width: 200px;
+`;
+
+
+export const CartImages = styled.img`
+ height : 100px;
+width : 80px; `
